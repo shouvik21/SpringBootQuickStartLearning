488512f561e1bbe3c6b60c45500a48e98e4afa28
shouvik21 <shouvikghosh14@gmail.com>
2022-12-05 23:07:08 +0530

    creating the project

---
+++ first.springProject/first.springProject/src/test/java/com/io/firstSpringProject/first/springProject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.firstSpringProject.first.springProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
